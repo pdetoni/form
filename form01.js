ff8ef74731d13b8c48f65ad2533635bb66c445d1
Pedro Detoni <pdpereira2003@gmail.com>
2022-07-19 20:30:03 -0300

    melhora função

--- form01.js
+++ form01.js
@@ -1,7 +1,14 @@
-function criaElemento(cor, largura, altura){
+document.exm01.addEventListener(`submit`, submitListener);  
+
+function criaElemento(cor, altura, largura){
     const enovo = document.createElement("div");
-    enovo.textContent=`${cor} ${largura} ${altura}`;
+    enovo.textContent=`${cor} ${altura} ${largura}`;
     document.body.appendChild(enovo);
 }
 
-criaElemento(document.exm01.cor.value, document.exm01.largura.value, document.exm01.altura.value);
+
+
+function submitListener(event){
+    event.preventDefault();
+criaElemento(document.exm01.cor.value, document.exm01.altura.value, document.exm01.largura.value);
+}
